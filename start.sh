252474ad35aa9a7eeca60d1e7cff10ad81e5a983
Shlok Bhakta <shlokbhakta1@gmail.com>
2025-05-13 02:11:14 -0500

    add docs to my scripts

--- start.sh
+++ start.sh
@@ -10,3 +10,9 @@ tmux new-window -n "backend"
 tmux send-keys "nix develop" C-m
 tmux send-keys "cd chess-backend" C-m
 tmux send-keys "flask run --debug" C-m
+
+tmux new-window -n "docs"
+tmux send-keys "nix develop" C-m
+tmux send-keys "cd chess-docs" C-m
+tmux send-keys "npm install" C-m
+tmux send-keys "npm run dev" C-m
--- stop.sh
+++ stop.sh
@@ -1,2 +1,3 @@
 tmux kill-window -t "backend"
 tmux kill-window -t "frontend"
+tmux kill-window -t "docs"
